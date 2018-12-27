fa6ebe1a7253a128b0275cce43813572d5d2cc4c
YuichiroSato <sato.yu1ro@gmail.com>
2018-12-28 00:23:08 +0900

    Add login form object

--- scala/web/chat/app/controllers/HomeController.scala
+++ scala/web/chat/app/controllers/HomeController.scala
@@ -1,11 +1,20 @@
 package controllers
 
 import javax.inject._
+import models.LoginData
 import play.api._
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.mvc._
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  val loginForm = Form(
+    mapping(
+      "userName" -> nonEmptyText,
+    )(LoginData.apply)(LoginData.unapply)
+  )
 
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
