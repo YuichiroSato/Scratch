d9a55853b7d2a4eebe0c867236474565d6ee37e1
YuichiroSato <sato.yu1ro@gmail.com>
2018-12-28 19:38:30 +0900

    Implement caching user name

--- scala/web/chat/app/controllers/HomeController.scala
+++ scala/web/chat/app/controllers/HomeController.scala
@@ -7,9 +7,11 @@ import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc._
 import play.api.i18n.I18nSupport
+import scala.concurrent.duration._
+import play.api.cache.redis.CacheApi
 
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
+class HomeController @Inject()(cc: ControllerComponents)(cache: CacheApi) extends AbstractController(cc) with I18nSupport {
 
   val loginForm = Form(
     mapping(
@@ -23,6 +25,26 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
 
   val login = Action(parse.form(loginForm)) { implicit request =>
     val loginData = request.body
+    var n = 0
+    var finished = false
+    while (!finished) {
+      val locked = cache.setIfNotExists("users-lock", "writing", 1.seconds)
+      if (locked) {
+        val isRegistered = cache.set[String]("users").contains(loginData.userName)
+        if (isRegistered) {
+          finished = true
+        } else {
+          cache.set[String]("users").add(loginData.userName)
+          finished = true
+        }
+        cache.remove("users-lock")
+      }
+      Thread.sleep(10)
+      n += 1
+      if (n >= 10) {
+        finished = true
+      }
+    }
     Ok(views.html.index(loginForm))
   }
 }
