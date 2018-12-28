8423ee0244b276cb9157522e47c0c8a00845675e
YuichiroSato <sato.yu1ro@gmail.com>
2018-12-28 13:24:49 +0900

    Path login form to index page

--- scala/web/chat/app/controllers/HomeController.scala
+++ scala/web/chat/app/controllers/HomeController.scala
@@ -3,12 +3,13 @@ package controllers
 import javax.inject._
 import models.LoginData
 import play.api._
-import play.api.data.Form
+import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc._
+import play.api.i18n.I18nSupport
 
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
 
   val loginForm = Form(
     mapping(
@@ -17,6 +18,6 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   )
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Ok(views.html.index(loginForm))
   }
 }
