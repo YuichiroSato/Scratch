2d097346df2d618248a19dcf707dc56eb52d52c6
YuichiroSato <sato.yu1ro@gmail.com>
2019-01-04 23:58:48 +0900

    Add chat room query

--- scala/web/chat/app/controllers/ListController.scala
+++ scala/web/chat/app/controllers/ListController.scala
@@ -1,15 +1,29 @@
 package controllers
 
 import javax.inject._
+import models.ChatRoom
+import org.joda.time.DateTime
 import play.api.mvc._
+import play.api.db.Database
+import anorm._
+import anorm.SqlParser._
 
 @Singleton
-class ListController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class ListController @Inject()(cc: ControllerComponents)(database: Database) extends AbstractController(cc) {
+
+  val chatRoomParser = get[Long]("room_id") ~ get[String]("description") ~ get[DateTime]("created_time")
 
   def index() = Action { implicit request: Request[AnyContent] =>
     val userName = request.session.get("userName")
     userName match {
-      case Some(_) => Ok(views.html.list())
+      case Some(_) => {
+        val result = database.withConnection { implicit conn =>
+          SQL("select * from rooms order by created_time desc")
+            .as(chatRoomParser.*)
+            .map { case room_id ~ description ~ created_time => ChatRoom(room_id, description, created_time) }
+        }
+        Ok(views.html.list(result))
+      }
       case None => Redirect(routes.HomeController.index)
     }
   }
