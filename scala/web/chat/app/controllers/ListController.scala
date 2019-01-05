263779a4c6651311c1b30cb47770f50978fe110a
YuichiroSato <sato.yu1ro@gmail.com>
2019-01-05 20:27:08 +0900

    Add chat room create form

--- scala/web/chat/app/controllers/ListController.scala
+++ scala/web/chat/app/controllers/ListController.scala
@@ -2,14 +2,24 @@ package controllers
 
 import javax.inject._
 import models.ChatRoom
+import models.ChatRoomData
 import org.joda.time.DateTime
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc._
 import play.api.db.Database
 import anorm._
 import anorm.SqlParser._
+import play.api.i18n.I18nSupport
 
 @Singleton
-class ListController @Inject()(cc: ControllerComponents)(database: Database) extends AbstractController(cc) {
+class ListController @Inject()(cc: ControllerComponents)(database: Database) extends AbstractController(cc) with I18nSupport {
+
+  val createChatRoomForm = Form(
+    mapping(
+      "description" -> nonEmptyText,
+    )(ChatRoomData.apply)(ChatRoomData.unapply)
+  )
 
   val chatRoomParser = get[Long]("room_id") ~ get[String]("description") ~ get[DateTime]("created_time")
 
@@ -22,9 +32,22 @@ class ListController @Inject()(cc: ControllerComponents)(database: Database) ext
             .as(chatRoomParser.*)
             .map { case room_id ~ description ~ created_time => ChatRoom(room_id, description, created_time) }
         }
-        Ok(views.html.list(result))
+        Ok(views.html.list(result)(createChatRoomForm))
       }
       case None => Redirect(routes.HomeController.index)
     }
+  }
+
+  val createChatRoom = Action(parse.form(createChatRoomForm)) { implicit request =>
+    val createRoomData = request.body
+    val result = database.withConnection { implicit conn =>
+      SQL("insert into rooms (description) values ({d})")
+        .on("d" -> createRoomData.description)
+        .execute()
+      SQL("select * from rooms order by created_time desc")
+        .as(chatRoomParser.*)
+        .map { case room_id ~ description ~ created_time => ChatRoom(room_id, description, created_time) }
+    }
+    Ok(views.html.list(result)(createChatRoomForm))
   }
 }
