93685c523b9e0dc3827e84b25f16096709b409d9
YuichiroSato <sato.yu1ro@gmail.com>
2019-01-06 02:24:20 +0900

    Add a controller for chat room

---
+++ scala/web/chat/app/controllers/RoomController.scala
@@ -0,0 +1,28 @@
+package controllers
+
+import javax.inject._
+import models.ChatRoom
+import org.joda.time.DateTime
+import play.api.mvc._
+import play.api.db.Database
+import anorm._
+import anorm.SqlParser._
+
+@Singleton
+class RoomController @Inject()(cc: ControllerComponents)(database: Database) extends AbstractController(cc) {
+
+  val chatRoomParser = get[Long]("room_id") ~ get[String]("description") ~ get[DateTime]("created_time")
+
+  def index(id: Long) = Action { implicit request: Request[AnyContent] =>
+    val room = database.withConnection { implicit conn =>
+      SQL("select * from rooms where room_id = {id}")
+        .on("id" -> id)
+        .as(chatRoomParser.*)
+        .map { case room_id ~ description ~ created_time => ChatRoom(room_id, description, created_time) }
+    }
+    room.headOption match {
+      case Some(_) => Ok(views.html.room())
+      case None => NotFound("")
+    }
+  }
+}
