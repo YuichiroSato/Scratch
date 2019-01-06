961b6dad762e6d9001e9dae1405d5cddb2605fb4
YuichiroSato <sato.yu1ro@gmail.com>
2019-01-06 15:34:30 +0900

    Add login check on chat room

--- scala/web/chat/app/controllers/RoomController.scala
+++ scala/web/chat/app/controllers/RoomController.scala
@@ -14,15 +14,21 @@ class RoomController @Inject()(cc: ControllerComponents)(database: Database) ext
   val chatRoomParser = get[Long]("room_id") ~ get[String]("description") ~ get[DateTime]("created_time")
 
   def index(id: Long) = Action { implicit request: Request[AnyContent] =>
-    val room = database.withConnection { implicit conn =>
-      SQL("select * from rooms where room_id = {id}")
-        .on("id" -> id)
-        .as(chatRoomParser.*)
-        .map { case room_id ~ description ~ created_time => ChatRoom(room_id, description, created_time) }
-    }
-    room.headOption match {
-      case Some(_) => Ok(views.html.room())
-      case None => NotFound("")
+    val userName = request.session.get("userName")
+    userName match {
+      case Some(_) => {
+        val room = database.withConnection { implicit conn =>
+          SQL("select * from rooms where room_id = {id}")
+            .on("id" -> id)
+            .as(chatRoomParser.*)
+            .map { case room_id ~ description ~ created_time => ChatRoom(room_id, description, created_time) }
+        }
+        room.headOption match {
+          case Some(_) => Ok(views.html.room())
+          case None => NotFound("")
+        }
+      }
+      case None => Redirect(routes.HomeController.index)
     }
   }
 }
