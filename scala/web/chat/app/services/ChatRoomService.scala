2e92f292019a175e97cbe3e0d15834b0669c2fe0
YuichiroSato <sato.yu1ro@gmail.com>
2019-01-25 18:25:25 +0900

    Add a method to get chat log

--- scala/web/chat/app/services/ChatRoomService.scala
+++ scala/web/chat/app/services/ChatRoomService.scala
@@ -4,8 +4,8 @@ import java.util.concurrent.ConcurrentHashMap
 
 import akka.stream.{KillSwitches, Materializer, UniqueKillSwitch}
 import akka.stream.scaladsl.{BroadcastHub, Flow, Keep, MergeHub}
-import play.api.libs.json.JsValue
 import javax.inject.{Inject, Singleton}
+import play.api.libs.json.{JsValue, Json}
 import play.api.cache.redis.CacheApi
 
 import scala.concurrent.duration._
@@ -13,6 +13,13 @@ import scala.concurrent.duration._
 @Singleton
 class ChatRoomService @Inject()(cache: CacheApi) {
   val rooms = new ConcurrentHashMap[Long, Flow[JsValue, JsValue, UniqueKillSwitch]]()
+
+  def getLog(id: Long) = {
+    val strPosts: Seq[String] = cache.list[String]("room_" + id).toList
+    val postArray = strPosts.map(Json.parse)
+        .foldLeft(Json.arr())((arr, j) => arr.append((j \ "text").get))
+    Json.obj("texts" -> postArray)
+  }
 
   def join(id: Long, materializer: Materializer) = {
     if (!rooms.containsKey(id)) {
