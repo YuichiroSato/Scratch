083a09d87cca8be1fdf6e4283fdeb92184dcafe7
YuichiroSato <sato.yu1ro@gmail.com>
2019-01-26 18:01:21 +0900

    Refactoring: applying ChatRoomRepository

--- scala/web/chat/app/controllers/ListController.scala
+++ scala/web/chat/app/controllers/ListController.scala
@@ -1,19 +1,16 @@
 package controllers
 
 import javax.inject._
-import models.ChatRoom
 import models.ChatRoomData
 import org.joda.time.DateTime
 import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc._
-import play.api.db.Database
-import anorm._
-import anorm.SqlParser._
 import play.api.i18n.I18nSupport
+import repositories.ChatRoomRepository
 
 @Singleton
-class ListController @Inject()(cc: ControllerComponents)(database: Database) extends AbstractController(cc) with I18nSupport {
+class ListController @Inject()(cc: ControllerComponents)(chatRoomRepository: ChatRoomRepository) extends AbstractController(cc) with I18nSupport {
 
   val createChatRoomForm = Form(
     mapping(
@@ -21,17 +18,11 @@ class ListController @Inject()(cc: ControllerComponents)(database: Database) ext
     )(ChatRoomData.apply)(ChatRoomData.unapply)
   )
 
-  val chatRoomParser = get[Long]("room_id") ~ get[String]("description") ~ get[DateTime]("created_time")
-
   def index() = Action { implicit request: Request[AnyContent] =>
     val userName = request.session.get("userName")
     userName match {
       case Some(_) => {
-        val result = database.withConnection { implicit conn =>
-          SQL("select * from rooms order by created_time desc")
-            .as(chatRoomParser.*)
-            .map { case room_id ~ description ~ created_time => ChatRoom(room_id, description, created_time) }
-        }
+        val result = chatRoomRepository.getRooms()
         Ok(views.html.list(result)(createChatRoomForm))
       }
       case None => Redirect(routes.HomeController.index)
@@ -40,14 +31,8 @@ class ListController @Inject()(cc: ControllerComponents)(database: Database) ext
 
   val createChatRoom = Action(parse.form(createChatRoomForm)) { implicit request =>
     val createRoomData = request.body
-    val result = database.withConnection { implicit conn =>
-      SQL("insert into rooms (description) values ({d})")
-        .on("d" -> createRoomData.description)
-        .execute()
-      SQL("select * from rooms order by created_time desc")
-        .as(chatRoomParser.*)
-        .map { case room_id ~ description ~ created_time => ChatRoom(room_id, description, created_time) }
-    }
+    chatRoomRepository.insert(createRoomData.description)
+    val result = chatRoomRepository.getRooms()
     Ok(views.html.list(result)(createChatRoomForm))
   }
 }
--- scala/web/chat/app/controllers/RoomController.scala
+++ scala/web/chat/app/controllers/RoomController.scala
@@ -5,35 +5,24 @@ import akka.actor._
 import akka.stream._
 import akka.stream.scaladsl._
 import javax.inject._
-import models.ChatRoom
-import org.joda.time.DateTime
 import play.api.mvc._
 import play.api.libs.streams.ActorFlow
-import play.api.db.Database
 import play.api.libs.json.JsValue
+import repositories.ChatRoomRepository
 import services.ChatRoomService
-import anorm._
-import anorm.SqlParser._
 
 
 @Singleton
-class RoomController @Inject()(cc: ControllerComponents, system: ActorSystem, materializer: Materializer, chatRoomService: ChatRoomService)(database: Database) extends AbstractController(cc) {
-
-  val chatRoomParser = get[Long]("room_id") ~ get[String]("description") ~ get[DateTime]("created_time")
+class RoomController @Inject()(cc: ControllerComponents, system: ActorSystem, materializer: Materializer, chatRoomService: ChatRoomService)(chatRoomRepository: ChatRoomRepository) extends AbstractController(cc) {
 
   def index(id: Long) = Action { implicit request: Request[AnyContent] =>
     val userName = request.session.get("userName")
     userName match {
       case Some(_) => {
-        val room = database.withConnection { implicit conn =>
-          SQL("select * from rooms where room_id = {id}")
-            .on("id" -> id)
-            .as(chatRoomParser.*)
-            .map { case room_id ~ description ~ created_time => ChatRoom(room_id, description, created_time) }
-        }
-        room.headOption match {
-          case Some(_) => Ok(views.html.room())
-          case None => NotFound("")
+        if (chatRoomRepository.isExists(id)) {
+          Ok(views.html.room())
+        } else {
+          NotFound("")
         }
       }
       case None => Redirect(routes.HomeController.index)
