1c4a94e758cfb960c42a17c6bb30cd9f96c29181
YuichiroSato <sato.yu1ro@gmail.com>
2019-01-26 18:00:24 +0900

    Add DI setting for ChatRoomRepository

--- scala/web/chat/app/Module.scala
+++ scala/web/chat/app/Module.scala
@@ -6,5 +6,6 @@ class Module extends AbstractModule {
   override def configure() = {
     bind(classOf[UserRepository]).to(classOf[RedisUserRepository])
     bind(classOf[UserLockRepository]).to(classOf[RedisUserLockRepository])
+    bind(classOf[ChatRoomRepository]).to(classOf[AnormChatRoomRepository])
   }
 }
